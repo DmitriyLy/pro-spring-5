8036ebcb3245e2ca372e631f367da0f7c88a0f6a
Dmytro Lysai <dmytro.lysai@netcracker.com>
2022-04-23 21:51:31 +0300

    ch16 - 678/697

---
+++ ch16_war/src/main/java/com/apress/prospring5/ch16/config/SecurityConfig.java
@@ -0,0 +1,7 @@
+package com.apress.prospring5.ch16.config;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    //TODO
+    //add spring security dependencies
+    //page 598/618
+}
